4f7787119af42ce7a38796256f0ab9f1732b139e
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-04-25 23:06:03 +0530

    finish frontend

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,146 +1,84 @@
-import React, { useState, useEffect } from "react";
-import { useContext } from "react";
-import AuthContext from '../../AuthContext'
-import { useNavigate, Navigate } from "react-router-dom";
-
-import { storage } from "../../firebase";
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import React, { useState,  } from "react";
+import Modal from "../Modal/Modal";
+import { FcPrevious } from "react-icons/fc";
+import { auth } from "../../firebase";
+import { useEffect } from "react";
+import note from "./../../assets/Sticky_Note.png"
+import Loading from "../Loading/Loading";
+import { Link } from "react-router-dom";
 
 
 const Dashboard = () => {
-  const [pdf, setPdf] = useState(null);
-  const [progressCircle, setProgressCircle] = useState("");
-  const [result, setResult] = useState("");
+  const [showMore, setShowMore] = useState(false);
+  const buttonText = showMore ? "Close" : "Create a new note";
+  const [userName, setUserName] = useState("")
+  const [isLoading, setIsLoading] = React.useState(true);
 
+  React.useEffect(() => {
+    setTimeout(() => { setIsLoading(false) }, 1600);
+  }, [])
 
-  const { user } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const file = event.target[0]?.files[0];
-
-    if (!file) {
-      return;
-    }
-
-    const storageRef = ref(storage, `files/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshots) => {
-        const progress = Math.round(
-          (snapshots.bytesTransferred / snapshots.totalBytes) * 100
-        );
-        setProgressCircle(progress);
-      },
-      (error) => {
-        alert(error);
-      },
-      () =>
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURl) => {
-          setPdf(downloadURl);
-        })
-    );
-
-    const startPage = document.getElementById("start-page").value;
-    const endPage = document.getElementById("end-page").value;
-
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("start_page", startPage);
-    formData.append("end_page", endPage);
-    console.log(file);
-
-
-    const response = await fetch("http://localhost:5000/upload", {
-      method: "POST",
-      body: formData,
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName)
+      }
+      else {
+        setUserName("")
+      }
     })
-    // .then(res => res.json())
-    // .then(data => {
-    //   console.log(data);
-    //   setResult(data);
-    // });
-
-    const data = await response.json()
-    setResult(data.extracted_text)
-
-  };
-
-  const handleNotimisation = () => {
-    if(result) {
-      navigate("/notimised?result="+ encodeURIComponent(result))
-    }
-    else {
-      alert("Please upload a pdf to process first")
-    }
-  }
+  
+  }, [])
 
 
-  return (
-    <div className="flex">
-      <h1>Profile</h1>
-
-      <div className="m-auto">
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col m-5 p-5 mt-[200px] "
-        >
-          <input
-            type="file"
-            name="pdf"
-            id=""
-            className="block w-full text-sm text-slate-500 file:rounded-full rounded-full"
-          />
-          <div className="flex flex-col">
-            {/* <label>Start Page:</label> */}
-            <input type="number" id="start-page" min="1" className="rounded-lg text-black m-2 p-2" placeholder="Start Page"/>
-            {/* <label>End Page:</label> */}
-            <input type="number" id="end-page" min="1" className="rounded-lg text-black m-2 p-2" placeholder="End Page"/>
-          </div>
-          <button
-            type="submit"
-            className="bg-white text-black  m-4 py-2 px-4 rounded-full"
-          >
-            Upload
-          </button>
-        </form>
-
-        {!pdf && (
-          <div>
-            <div className="">
-              {progressCircle && `${progressCircle} Notimising....`}{" "}
-            </div>
-          </div>
-        )}
-
-        {pdf && (
-          <div className="">
-            <button className="bg-white text-black  m-4 py-2 px-4 rounded-full">
-              <a href={pdf} target="_blank" className="">
-                View File
-              </a>
-            </button>
-
-            <button
-              type="submit"
-              onClick={handleNotimisation}
-              className="bg-white text-black  m-4 py-2 px-4 rounded-full"
-            >
-              View Notimised
-            </button>
-
-          </div>
-        )}
-        {result && <h1>{result.message}</h1>}
-
+  if (isLoading) {
+    return (
+      <div className="bg-background w-full overflow-hidden">
+        <Loading />
       </div>
+    )
+  } else {
+  return (
+    <div>
+      <header className="h-[80px] w-full bg-[#2abfff] font-bold p-5 text-2xl text-white flex justify-between items-center">
+        <p>Welcome {userName}</p>
+        <Link to='/login'><p className="justify-end text-xl border-2 border-white p-2 rounded-md hover:bg-white hover:text-[#2abfff]">Logout</p></Link>
+      </header>
+      {showMore ? (
+                <div className=" flex w-[90%] sm:w-[70%] md:w-[60%] h-[70%] m-auto my-20 p-2 shadow-2xl rounded-3xl opacity-90">
+                  <div className="w-full h-full flex-initial justify-center">
+                    <div className="basis-11/12 flex py-2">
+                      <button
+                        className="hover:bg-[#2abfff] rounded p-2 inset-x-100"
+                        onClick={() => {
+                          setShowMore(!showMore);
+                        }}
+                      >
+                        <FcPrevious className="w-[30px]" />
+                      </button>
+                    </div>
+                    <div className="basis-11/12 flex py-2">
+                      <Modal />
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="h-screen flex">
+                  <div className="flex justify-center mx-auto my-20">
+                    <button
+                      className="w-[400px] h-[500px] border-2 rounded-lg"
+                      onClick={() => {
+                        setShowMore(!showMore);
+                      }}
+                    >
+                      <img src={note} alt="note" className="w-[200px] h-[200px] mt-[-100px] mx-auto" />
+                      <p className="text-[#2abfff] font-bold text-2xl mt-10">{buttonText}</p>
+                    </button>
+                  </div>
+                </div>
+              )}
     </div>
-  );
+  );}
 };
 
 export default Dashboard;
---
+++ client/src/components/Modal/Modal.jsx
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import { useContext } from "react";
+import AuthContext from '../../AuthContext'
+import { useNavigate, Navigate } from "react-router-dom";
+import img from '../../assets/split-pdf.svg'
+import { storage } from "../../firebase";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
+
+
+const Modal = () => {
+    const [showModal,setShowModal]=useState(false);
+    const open=()=>{
+    setShowModal(!showModal);
+    }
+    const [pdf, setPdf] = useState(null);
+    const [progressCircle, setProgressCircle] = useState("");
+    const [result, setResult] = useState("");
+    const [show, setShow] = useState(false);
+  
+  
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+  
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      setShow(true);
+  
+      const file = event.target[0]?.files[0];
+  
+      if (!file) {
+        return;
+      }
+  
+      const storageRef = ref(storage, `files/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+  
+      uploadTask.on(
+        "state_changed",
+        (snapshots) => {
+          const progress = Math.round(
+            (snapshots.bytesTransferred / snapshots.totalBytes) * 100
+          );
+          setProgressCircle(progress);
+        },
+        (error) => {
+          alert(error);
+        },
+        () =>
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURl) => {
+            setPdf(downloadURl);
+          })
+      );
+  
+      const startPage = document.getElementById("start-page").value;
+      const endPage = document.getElementById("end-page").value;
+  
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("start_page", startPage);
+      formData.append("end_page", endPage);
+      console.log(file);
+  
+  
+      const response = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData,
+      })
+  
+      const data = await response.json()
+      setResult(data.extracted_text)
+  
+    };
+  
+    const handleNotimisation = () => {
+      if(result) {
+        navigate("/notimised?result="+ encodeURIComponent(result))
+      }
+      else {
+        alert("Please upload a pdf to process first")
+      }
+    }
+
+  return (
+        <div className="flex flex-col mx-auto h-[600px]">
+          {!show? (
+        <>
+          <img src={img} className="mx-auto p-0 w-[60%]" alt="" />
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col m-5 p-5 mt-[50px] "
+        >
+          <input
+            type="file"
+            name="pdf"
+            id=""
+            required
+            className="p-3 pl-5 text-lg bg-white border-2 border-white my-1 rounded-lg text-[#2abfff] placeholder:text-[#2abfff] w-[100%]"
+          />
+          <div className="flex">
+            
+            <input type="number" id="start-page" min="1" className="m-2 p-3 text-lg bg-white border-2 my-1 rounded-lg text-[#2abfff] placeholder:text-[#2abfff] w-[50%]" placeholder="Start Page"/>
+            <input type="number" id="end-page" min="1" className="m-2 p-3 text-lg bg-white border-2  my-1 rounded-lg text-[#2abfff] placeholder:text-[#2abfff] w-[50%]" placeholder="End Page"/>
+          </div>
+          <button
+            type="submit"
+            className="p-3 my-10 text-lg text-white border-2 border-white rounded-lg bg-[#2abfff] w-[100%]"
+          >
+            NOTIMISE
+          </button>
+        </form>
+        </>):(
+        <div className="flex flex-col mx-auto">
+          <div className="w-[150px] justify-center mx-auto mt-[100px]">
+          <CircularProgressbarWithChildren value={progressCircle} strokeWidth={6} styles={buildStyles({ pathColor: "#2abfff", trailColor: "#eee" })}>
+        <div>{`${progressCircle}%`}</div>
+      </CircularProgressbarWithChildren>
+      </div>
+
+        {!pdf?(
+          <div>
+            <p className="text-[#2abfff] font-bold my-10 text-center text-xl">
+              {progressCircle && `Notimising....`}{" "}
+            </p>
+          </div>
+        ):(
+          <div className="text-[#2abfff] text-xl">
+            
+            
+      <div className="mx-auto justify-center">
+            <p className="text-[#2abfff] font-bold text-center my-10 text-xl">Notimised</p>
+            <button className="m-4  hover:bg-[#2abfff] border-2 border-[#2abfff] hover:text-white rounded-md p-2">
+              <a href={pdf} target="_blank" className="">
+                Uploaded File
+              </a>
+            </button>
+            <button
+              type="submit"
+              onClick={handleNotimisation}
+              className="m-4  hover:bg-[#2abfff] border-2 border-[#2abfff] hover:text-white rounded-md p-2"
+            >Notimised File</button>
+
+          </div>
+          </div>
+        )}
+        {result && <h1>{result.message}</h1>}
+        </div>)}
+      </div>
+  );
+};
+
+export default Modal;
