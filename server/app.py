117eef5a08025a772499c079184fbd0f30e3d115
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-04-25 12:53:12 +0530

    upload pdf and extract

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, request
 from flask_cors import CORS
+from PyPDF2 import PdfReader
 
 app = Flask(__name__) 
 CORS(app) 
@@ -9,4 +10,16 @@ CORS(app)
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    return {"message": "HackToFuture"}
+    uploaded_file = request.files['file']
+    start_page = int(request.form['start_page'])
+    end_page = int(request.form['end_page'])
+    pdf_reader = PdfReader(uploaded_file)
+    num_pages = len(pdf_reader.pages)
+    if end_page > num_pages:
+        end_page = num_pages
+    text = ""
+    for page in range(start_page-1, end_page):
+        text += pdf_reader.pages[page].extract_text()
+
+    print(text)
+    return {"message": "read pdf successfully"}
