0291d5dcb3cbcc5f285f3672ca56b236fdf49703
unknown <gandhi.mardav@gmail.com>
2023-04-25 16:09:43 +0530

    firebase notimise

--- client/src/components/Notimised/Notimised.js
+++ client/src/components/Notimised/Notimised.js
@@ -1,13 +1,19 @@
 import { useLocation } from 'react-router-dom';
 import jsPDF from 'jspdf';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../../firebase';
 
 const Notimised = () => {
   const location = useLocation();
   const result = new URLSearchParams(location.search).get('result');
   const notimisedRef = useRef(result)
 
-  const handleGeneratePDF = () => {
+  const [notimisedFB, setNotimisedFB] = useState(null)
+
+  const handleGeneratePDF = (e) => {
+
+    e.preventDefault()
 
     const doc = new jsPDF({
 			format: 'a4',
@@ -16,9 +22,28 @@ const Notimised = () => {
 
     doc.html(notimisedRef.current, {
 			async callback(doc) {
-				await doc.save(`note-heading`);
+				await doc.save(`1`);
 			},
     })
+
+    const file = e.target[0]?.files[0]
+
+    if(!file) {
+      return
+    }
+
+    const storageRef = ref(storage, `notimised/${file.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", 
+    (error) => {
+      alert(error)
+    },
+
+    () => getDownloadURL(uploadTask.snapshot.ref).then((downloadURl) => {
+      setNotimisedFB(downloadURl)
+    })
+    )
 
   }
 
@@ -28,6 +53,11 @@ const Notimised = () => {
       <button onClick={handleGeneratePDF}>
         Generate PDF
       </button>
+
+      {notimisedFB && 
+      <a href={notimisedFB}>View PDF</a>
+      }
+
       <div className='m-auto p-4 w-[500px] border-2 border-white rounded-lg'>
       <p className='italic text-amber-950 font-mono font-extrabold' ref={notimisedRef}>{result}</p>
       </div>
